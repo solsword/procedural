dc13dc3e1ad94cdaa3cfd3f7cf62ca45de9b6dd9
Peter Mawhorter <pmawhorter@gmail.com>
2019-08-08 15:54:53 -0400

    Got syntax highlighting working, and also got error reporting to work reasonably (better error descriptions and more detail would be ideal). Next step is to actually run our unit tests and report when the puzzle is solved!
    -Peter

--- web/procedural.py
+++ web/procedural.py
@@ -71,12 +71,14 @@ def drag_start(ev):
   button held down. The element under the cursor becomes the event's target.
   """
   global DRAGGED
+  # okay to use this here because things inside aren't draggable, so event will
+  # bubble to the code_block div, which is.
   if not has_class(ev.target, "code_block"):
     ev.preventDefault()
     return False
 
   DRAGGED = ev.target
-  DRAGGED.style.opacity = 0.6
+  add_class(DRAGGED, "dragging")
   return False
 
 def drag_end(ev):
@@ -85,7 +87,7 @@ def drag_end(ev):
   """
   global DRAGGED
   if DRAGGED != None:
-    DRAGGED.style.opacity = "";
+    remove_class(DRAGGED, "dragging")
     DRAGGED = None
   ev.preventDefault()
   return False
@@ -96,11 +98,18 @@ def drag_enter(ev):
   new element (the target).
   """
   global DRAGGED
+
   if not same_widget(ev.target, DRAGGED):
     return False
-  if has_class(ev.target, "code_slot") or has_class(ev.target, "code_block"):
+
+  if has_class(ev.target, "code_slot"):
+    block = ev.target
+  else:
+    block = my_code_block(ev.target)
+
+  if block != None and not block.isSameNode(DRAGGED):
     ev.dataTransfer.dropEffect = "move"
-    add_class(ev.target, "hovered")
+    add_class(block, "hovered")
 
 def drag_leave(ev):
   """
@@ -108,11 +117,22 @@ def drag_leave(ev):
   element (the target) that it was previously over.
   """
   global DRAGGED
+
   if not same_widget(ev.target, DRAGGED):
     return False
-  if has_class(ev.target, "code_slot") or has_class(ev.target, "code_block"):
-    ev.dataTransfer.dropEffect = "none"
-    remove_class(ev.target, "hovered")
+
+  if has_class(ev.target, "code_slot"):
+    block = ev.target
+  else:
+    block = my_code_block(ev.target)
+
+  if block != None:
+    # Block that we're leaving to:
+    to_block = my_code_block(ev.relatedTarget)
+
+    if not block.isSameNode(to_block):
+      ev.dataTransfer.dropEffect = "none"
+      remove_class(block, "hovered")
 
 def drag_over(ev):
   """
@@ -132,34 +152,51 @@ def drag_drop(ev):
   if not same_widget(ev.target, DRAGGED):
     ev.preventDefault()
     return False
-  is_block = has_class(ev.target, "code_block")
-  if has_class(ev.target, "code_slot") or is_block:
+  my_block = my_code_block(ev.target)
+  if has_class(ev.target, "code_slot") or my_block != None:
     # drop on a slot: insert ourselves after the slot
-    slot = ev.target
-    block = DRAGGED
-    if slot == block: # drop on ourselves: do nothing
+    slot = my_block or ev.target
+    if slot.isSameNode(DRAGGED): # drop on ourselves: do nothing
       ev.preventDefault()
       remove_class(slot, "hovered")
       return
-    if is_block:
-      before = block.previousSibling
-    block.parentNode.removeChild(block);
-    if is_block and slot == before:
-      # if we're dropping on a block and it's the block above us, we should
+    if my_block != None:
+      after = DRAGGED.nextSibling
+    DRAGGED.parentNode.removeChild(DRAGGED)
+    if my_block != None and slot.isSameNode(after):
+      # if we're dropping on a block and it's the block below us, we should
       # swap places instead of going nowhere:
-      slot.parentNode.insertBefore(block, before)
+      slot.parentNode.insertBefore(DRAGGED, after.nextSibling)
     else:
-      # otherwise just add ourselves after the target:
-      slot.parentNode.insertBefore(block, slot.nextSibling)
-    # Note: it's ok if nextSibling is None because insertBefore defaults to
-    # append if the second argument is None.
+      # otherwise just add ourselves before the target:
+      slot.parentNode.insertBefore(DRAGGED, slot)
     # now clean up classes:
     remove_class(slot, "hovered")
+
+    # Mark errors on this widget as stale:
+    mark_errors_as_stale(my_widget(ev.target))
+
+  elif has_class(ev.target, "code_bucket"):
+    # drop on a bucket: add ourselves after the last code block in that bucket:
+    last = None
+    for child in ev.target.children:
+      if has_class(child, "code_slot", "code_block"):
+        last = child
+    if last == None:
+      DRAGGED.parentNode.removeChild(DRAGGED)
+      ev.target.insertBefore(DRAGGED, ev.target.firstChild)
+    else:
+      DRAGGED.parentNode.removeChild(DRAGGED)
+      ev.target.insertBefore(DRAGGED, last.nextSibling)
+
+    # Mark errors on this widget as stale:
+    mark_errors_as_stale(my_widget(ev.target))
+
   else:
     ev.preventDefault()
 
   # reset dragged element's style and then get rid of it:
-  DRAGGED.style.opacity = "";
+  remove_class(DRAGGED, "dragging")
   DRAGGED = None
 
   return False
@@ -177,17 +214,19 @@ def create_slot():
   slot.innerHTML = "&nbsp;" # don't let it be completely empty
   return slot
 
-def add_code_block_to_bucket(bucket, block):
+def add_code_block_to_bucket(bucket, code):
   """
   Adds a block of code to a code block bucket. Creates the requisite DOM
   element and translates from raw code to HTML specifics. The given bucket
   element should be a DOM element with the code_bucket class.
   """
-  codeblock = document.createElement("div")
+  codeblock = document.createElement("code")
   add_class(codeblock, "code_block")
+  add_class(codeblock, "language-python")
   codeblock.draggable = True
-  codeblock.__code__ = block
-  codeblock.innerHTML = block
+  codeblock.__code__ = code
+  codeblock.innerHTML = code
+  browser.window.Prism.highlightElement(codeblock)
   bucket.appendChild(codeblock)
 
 def add_empty_slot_to_bucket(bucket):
@@ -209,16 +248,24 @@ def my_widget(node):
   else:
     return None
 
+def my_code_block(node):
+  """
+  Figure out which code block a DOM node belongs to. Recursively asks parent
+  DOM node until a node with class code_block is found.
+  """
+  if has_class(node, "code_block"):
+    return node
+  elif hasattr(node, "parentNode"):
+    return my_code_block(node.parentNode)
+  else:
+    return None
+
 def same_widget(a, b):
   """
   Generic function for asking if two elements belong to the same widget or not.
   Just uses my_widget on each and compares results.
   """
-  # TODO: check that this isn't vulnerable to collisions?
-  #mwa = my_widget(a)
-  #mwb = my_widget(b)
-  #log(dir(mwa), dir(mwb), id(mwa), id(mwb), mwa == mwb)
-  return id(my_widget(a)) == id(my_widget(b))
+  return my_widget(a) is my_widget(b)
 
 #----------------------#
 # Evaluation Functions #
@@ -283,13 +330,13 @@ def setup_widget(node, puzzle=None):
   setup_base_puzzle(node, puzzle) # accepts None puzzle and defaults
 
   # Set up event handlers on this widget
-  node.addEventListener("dragstart", drag_start, False);
-  node.addEventListener("dragend", drag_end, False);
-  node.addEventListener("dragover", drag_over, False);
-  node.addEventListener("dragenter", drag_enter, False);
-  node.addEventListener("dragenter", drag_enter, False);
-  node.addEventListener("dragleave", drag_leave, False);
-  node.addEventListener("drop", drag_drop, False);
+  node.addEventListener("dragstart", drag_start, False)
+  node.addEventListener("dragend", drag_end, False)
+  node.addEventListener("dragover", drag_over, False)
+  node.addEventListener("dragenter", drag_enter, False)
+  node.addEventListener("dragenter", drag_enter, False)
+  node.addEventListener("dragleave", drag_leave, False)
+  node.addEventListener("drop", drag_drop, False)
 
 def blocks_from_lines(code):
   """
@@ -308,12 +355,15 @@ def eval_button_handler(ev):
   """
   Click handler for the evaluate button of a puzzle.
   """
+  # TODO: activity indicator
   bucket = ev.target.__bucket__
-  code = "";
+  remove_errors(my_widget(bucket))
+  code = ""
   for child in bucket.children:
     if child.hasOwnProperty("__code__"):
       code += child.__code__ + '\n'
-  log("Code:\n{}".format(code))
+  code = code[:-1] # remove trailing newline
+  log("Running code:\n---\n{}\n---".format(code))
   try:
     env = exec_code(code)
   # TODO: handle syntax errors separately?
@@ -321,28 +371,87 @@ def eval_button_handler(ev):
   except Exception as e:
     tbe = traceback.TracebackException(*sys.exc_info())
     log("Result was an exception:\n" + ''.join(tbe.format()))
-    block = block_responsible_for(bucket, tbe)
-    add_class(block, "error")
+    attach_error_mesage(bucket, tbe)
 
-    err = document.createElement("div")
-    err.innerHTML = list(tbe.format())[-1].strip()
-    block.appendChild(err)
+def attach_error_mesage(bucket, tbe):
+  """
+  Given a code bucket (that was just evaluated) and a TracebackException object
+  (which resulted from that evaluation), this method generates an error DOM
+  node and attaches it to the relevant code block in the given bucket.
+  """
+  block, line = block_and_line_responsible_for(bucket, tbe)
+  exc_msg = ':'.join(list(tbe.format())[-1].split(':')[1:]).strip()
 
-def block_responsible_for(bucket, tbe):
+  err = document.createElement("details")
+  add_class(err, "error")
+  err.innerHTML = exc_msg
+  errname = document.createElement("summary")
+  errname.innerHTML = tbe.exc_type.__name__
+  err.appendChild(errname)
+  if tbe.exc_type == SyntaxError:
+    err.appendChild(
+      document.createTextNode(
+        "\nThe error was detected at this point:"
+      )
+    )
+    errdesc = document.createElement("pre")
+    add_class(errdesc, "syntax-error-description")
+    errcode = document.createElement("code")
+    add_class(errcode, "language-python")
+    errcode.innerHTML = tbe.text
+    browser.window.Prism.highlightElement(errcode) # highlight just the code
+    errdesc.appendChild(errcode)
+    # add the caret and message after the code
+    caret_text = '\n' + list(tbe.format())[-2]
+    #caret_text = '\n' + ('&nbsp;' * tbe.offset) + '^'
+    caret = document.createTextNode(caret_text)
+    errdesc.appendChild(caret)
+    err.appendChild(errdesc)
+
+  elif '\n' in block.__code__: # multi-line code so identify the line
+    err.appendChild(
+      document.createTextNode(
+        "\nThe error was detected on this line:"
+      )
+    )
+    err_line = block.__code__.split('\n')[line]
+    errdesc = document.createElement("pre")
+    add_class(errdesc, "syntax-error-description")
+    errcode = document.createElement("code")
+    add_class(errcode, "language-python")
+    errcode.innerHTML = err_line
+    browser.window.Prism.highlightElement(errcode) # highlight just the code
+    errdesc.appendChild(errcode)
+    err.appendChild(errdesc)
+
+  block.appendChild(err)
+
+def block_and_line_responsible_for(bucket, tbe):
   """
   Figures out which block of code in a bucket was responsible for the given
   traceback by counting code lines and looking at the traceback's final line
-  number.
+  number. Also returns the line number within that block, as the second part of
+  a tuple. Logs an error and returns None if the line number is out of range
+  for the code block.
   """
-  line = tbe.stack[-1].lineno
+  if tbe.exc_type == SyntaxError:
+    line = int(tbe.lineno)
+  else:
+    line = tbe.stack[-1].lineno
   sofar = 0
+  last = None
+  lines = None
   for child in bucket.children:
     if child.hasOwnProperty("__code__"):
+      last = child
       lines = len(child.__code__.split('\n'))
       if sofar + lines >= line:
-        return child
+        return (child, line - 1 - sofar)
       else:
         sofar += lines
+  if line == sofar + 1 and last != None:
+    # e.g., an indentation error on final added blank line
+    return (last, lines - 1)
   error("Ran out of code lines trying to find responsible block!")
   error(
     "children: {}, lines: {}, target: {}".format(
@@ -353,27 +462,46 @@ def block_responsible_for(bucket, tbe):
   )
   return None
 
+def mark_errors_as_stale(widget):
+  """
+  Marks all errors in the given widget as stale (presumably because they're no
+  longer 100% valid as code has been moved around).
+  """
+  for node in widget["node"].querySelectorAll(".error"):
+    add_class(node, "stale")
+
+def remove_errors(widget):
+  """
+  Removes errors from the given widget (presumably in preparation for
+  re-executing the code and generating new errors).
+  """
+  for node in widget["node"].querySelectorAll(".error"):
+    node.parentNode.removeChild(node)
+
 def setup_base_puzzle(node, puzzle):
   """
   Sets up a basic two-column puzzle where you drag blocks from the left into
-  numbered slots on the right.
+  blank space on the right.
   """
   if puzzle == None:
     puzzle = { # default puzzle:
       "code": [
-        "a = 3;",
-        "b = 4;",
-        "c = a*b;",
+        "a = 3",
+        "b = 4",
+        "c = a*b",
         "a += 5",
-        "b = a + 1;",
-        "c = c + a;"
+        "b = a + 1",
+        "c = c + a"
       ],
       "pretest": "",
       "tests": [ "a == 8", "b == 9", "c == 20" ],
       "goal": "Rearrange the code so that all of the tests are satisfied.",
       "show_tests": True
     }
-  w = { "puzzle": puzzle } # the widget object
+  w = {
+    "puzzle": puzzle,
+    "node": node
+  } # the widget object
   node.__widget__ = w # attach it to the DOM
 
   code_blocks = puzzle["code"]
@@ -383,7 +511,7 @@ def setup_base_puzzle(node, puzzle):
   w["code_blocks"] = code_blocks
 
   # bucket for source blocks
-  w["source_bucket"] = document.createElement("div");
+  w["source_bucket"] = document.createElement("div")
   add_class(w["source_bucket"], "code_bucket", "code_source")
   node.appendChild(w["source_bucket"])
 
@@ -393,9 +521,6 @@ def setup_base_puzzle(node, puzzle):
   random.seed(seed)
   random.shuffle(code_blocks)
 
-  # First add a source slot so that we can drag things back if it's empty:
-  add_empty_slot_to_bucket(w["source_bucket"])
-
   # Add each code block to our source div:
   for block in code_blocks:
     add_code_block_to_bucket(w["source_bucket"], block)
@@ -403,18 +528,16 @@ def setup_base_puzzle(node, puzzle):
   # bucket for solution blocks
   w["soln_bucket"] = document.createElement("div")
   add_class(w["soln_bucket"], "code_bucket", "soln_list")
-  node.appendChild(w["soln_bucket"]);
-
-  # Create the answer slot:
-  add_empty_slot_to_bucket(w["soln_bucket"])
+  node.appendChild(w["soln_bucket"])
 
   # Create evaluate button in solution bucket:
-  eb = document.createElement("input");
-  eb.type = "button";
-  eb.value = "evaluate";
-  eb.__bucket__ = w["soln_bucket"];
+  eb = document.createElement("input")
+  add_class(eb, "eval-button")
+  eb.type = "button"
+  eb.value = "check"
+  eb.__bucket__ = w["soln_bucket"]
   eb.addEventListener("click", eval_button_handler)
-  w["soln_bucket"].appendChild(eb);
+  w["soln_bucket"].appendChild(eb)
 
 
 def sequence_puzzles(widget, puzzles):
@@ -422,8 +545,8 @@ def sequence_puzzles(widget, puzzles):
   Takes a list of puzzle objects and presents them one at a time through the
   given widget, displaying a new puzzle when the current one is solved.
   """
-  #log(puzzles[0]);
-  setup_widget(widget, puzzles[0]);
+  #log(puzzles[0])
+  setup_widget(widget, puzzles[0])
   # TODO: HERE
 
 def load_puzzles(url, continuation):
@@ -440,10 +563,10 @@ def load_puzzles(url, continuation):
   """
   # TODO: this?!?
   #xobj = browser.ajax.Ajax()
-  #xobj.overrideMimeType("application/json");
-  base = window.location.href;
+  #xobj.overrideMimeType("application/json")
+  base = window.location.href
   path = '/'.join(base.split('/')[:-1])
-  dpath = path + "/" + url;
+  dpath = path + "/" + url
 
   # Load asynchronously
   def catch(req):
@@ -478,7 +601,7 @@ def init():
   """
 
   # Collect each widget:
-  widgets = document.querySelectorAll(".procedural_widget");
+  widgets = document.querySelectorAll(".procedural_widget")
 
   # Set up each widget automatically:
   #for widget in widgets:
