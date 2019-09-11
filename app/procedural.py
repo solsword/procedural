3f6e722dfe971d22ae22b763609796538be19eb7
Peter Mawhorter <pmawhorter@gmail.com>
2019-09-10 22:11:14 -0400

    Updates to permissions and submissions that just use flat files instead of databases! Still incomplete, and of course on the wrong branch T_T
    -Peter Mawhorter

--- app/procedural.py
+++ app/procedural.py
@@ -14,7 +14,6 @@ import werkzeug
 import os
 import sys
 import json
-import sqlite3
 import traceback
 
 #------------------#
@@ -35,12 +34,6 @@ CSP = {
   ],
 }
 
-# Solutions database filename
-SOL_DATABASE = "solutions.sqlite3"
-
-# Permissions database filename
-PERM_DATABASE = "permissions.sqlite3"
-
 # Database schema
 SOL_SCHEMA = """
 CREATE TABLE solutions (
@@ -52,14 +45,8 @@ CREATE TABLE solutions (
 );
 """
 
-PERM_SCHEMA = """
-CREATE TABLE permissions (
-  username TEXT UNIQUE NOT NULL,
-  is_admin TEXT,
-  permissions TEXT
-);
-"""
-
+# Default permissions
+DEFAULT_PERMISSIONS = { "admins": [], "puzzles": {} }
 
 #-------------------------#
 # Setup and Configuration #
@@ -105,8 +92,8 @@ def returnJSON(f):
 def admin_only(f):
   """
   Wraps a function so that it first checks the CAS username against the
-  permissions database and retursn 403 (forbidden) if the user isn't set up as
-  an admin.
+  permissions file and returns 403 (forbidden) if the user isn't set up
+  as an admin.
   """
   def wrapped(*args, **kwargs):
     username = flask.session.get("CAS_USERNAME", None)
@@ -154,8 +141,8 @@ def route_puzzle():
   """
   This route returns JSON puzzles from the PUZZLES_DIRECTORY.
   """
-  id = flask.request.form.get("id", None)
-  if id == None:
+  puzzle_id = flask.request.form.get("id", None)
+  if puzzle_id == None:
     return { # default puzzle:
       "id": "default_server_puzzle",
       "name": "Default Server Puzzle",
@@ -174,12 +161,12 @@ def route_puzzle():
       ]
     }
   else:
-    id = werkzeug.utils.secure_filename(id)
-    if '.' in id:
-      return ("Invalid puzzle ID: '{}'".format(id), 400)
+    puzzle_id = werkzeug.utils.secure_filename(puzzle_id)
+    if '.' in puzzle_id:
+      return ("Invalid puzzle ID: '{}'".format(puzzle_id), 400)
     user = flask.session.get("CAS_USERNAME", None)
-    if has_permission(user, "view_puzzle", id):
-      bits = id.split('-')
+    if has_permission(puzzle_id, user):
+      bits = puzzle_id.split('-')
       pdir = app.config.get("PUZZLES_DIRECTORY", "puzzles")
       target = os.path.join(pdir, *bits) + ".json"
       if os.path.exists(target):
@@ -187,9 +174,14 @@ def route_puzzle():
           puzzle = fin.read()
         return puzzle
       else:
-        return ("Puzzle '{}' does not exist.".format(id), 404)
+        return ("Puzzle '{}' does not exist.".format(puzzle_id), 404)
     else:
-      return ("You don't have permissions to view puzzle '{}'.".format(id), 403)
+      return (
+        "You don't have permissions to view puzzle '{}'.".format(
+          puzzle_id
+        ),
+        403
+      )
 
 @app.route("/solved", methods=["POST"])
 @returnJSON
@@ -248,318 +240,270 @@ def route_solved():
 
 def record_solution(username, puzzle, solution):
   """
-  Records a solution in the database.
+  Records a solution in the solutions directory. Returns True if it
+  succeeds and False if it fails.
   """
-  conn = get_sol_db_connection()
-  conn.execute(
-    "INSERT INTO solutions VALUES (?, DATETIME('now'), ?, ?, ?);",
-    (
-      username,
-      puzzle.get("id", "__unknown__"),
-      json.dumps(puzzle),
-      json.dumps(solution)
-    )
+  # create solutions directory if necessary
+  sd = app.config.get("SOLUTIONS_DIR", "submissions")
+  try:
+    if not os.path.exists(sd):
+      os.mkdir(sd, mode=0o770)
+  except:
+    print("Failed to create submissions directory '{}'.".format(sd))
+    return False
+
+  # create user directory if necessary
+  ud = os.path.join(sd, username)
+  try:
+    if not os.path.exists(ud):
+      os.mkdir(ud, mode=0o770)
+  except:
+    print("Failed to create user subdirectory '{}'.".format(ud))
+    return False
+
+  # timestamp
+  ts = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f")
+  # full filename
+  fn = "{}-solution:{}.json".format(
+    puzzle.get("id", "__unknown__"),
+    ts
   )
-  conn.commit()
-  conn.close()
+  # with path
+  sf = os.path.join(ud, fn)
+
+  # write the solution into a file
+  try:
+    with open(sf, 'w') as fout:
+      json.dump({"puzzle": puzzle, "solution": solution}, fout)
+  except:
+    print("Failed to write solution file '{}'.".format(sf))
+    return False
+
+  # we succeeded!
   return True
 
 def all_solutions_by(username):
   """
-  Retrieves from the database a list of all solutions by the given user. The
-  return value is a list of sqlie3 Row objects.
+  Retrieves from the solutions directory a list of all solutions by the
+  given user. The return value is a list of full filenames.
   """
-  conn = get_sol_db_connection()
-  cur = conn.execute("SELECT * FROM solutions WHERE username = ?;", (username,))
-  result = list(cur.fetchall())
-  conn.close()
+  sd = app.config.get("SOLUTIONS_DIR", "submissions")
+  ud = os.path.join(sd, username)
+
+  if not os.path.exists(ud):
+    return []
+
+  result = []
+  for f in os.listdir(ud):
+    ff = os.path.join(ud, f)
+    if os.path.isfile(ff):
+      result.append(ff)
+
   return result
 
 def all_solutions_to(puzzle_id):
   """
-  Retrieves from the database a list of all solutions to the given puzzle. The
-  return value is a list of sqlie3 Row objects.
+  Scans solution files to list of all solutions to the given puzzle by
+  any user. The return value is a list of pairs of username and
+  submission filename (potentially with multiple files submitted per
+  user).
   """
-  conn = get_sol_db_connection()
-  cur = conn.execute(
-    "SELECT * FROM solutions WHERE puzzle_id = ?;",
-    (puzzle_id,)
-  )
-  result = list(cur.fetchall())
-  conn.close()
+  result = []
+  sd = app.config.get("SOLUTIONS_DIR", "submissions")
+  if not os.path.exists(sd):
+    return []
+
+  # Loop over user directories
+  for dirname in os.listdir(sd):
+    ud = os.path.join(sd, dirname)
+    if os.path.isdir(ud):
+      # Loop over files for this user:
+      # TODO: wildcard based on puzzle ID should be much faster?
+      for filename in os.listdir(ud):
+        ff = os.path.join(ud, filename)
+        # check for submissions that match our puzzle ID:
+        if os.path.isfile(ff) and "-solution:" in filename:
+          fpid = filename[:filename.index("-solution:")]
+          if fpid == puzzle_id:
+            result.append((dirname, ff))
+
   return result
 
 def is_admin(user_id):
   """
-  Retrieves a user's admin status from the permissions database.
+  Retrieves a user's admin status from the permissions file.
   """
-  conn = get_perm_db_connection()
-  cur = conn.execute(
-    "SELECT is_admin FROM permissions WHERE username = ?;",
-    (user_id,)
-  )
-  results = list(cur.fetchall())
-  conn.close()
+  perms = get_current_permissions()
+  return user_id in perms["admins"]
 
-  return (
-    len(results) > 0
-and results[0][0] == "True"
-  )
+def get_permisisons(puzzle_id):
+  """
+  Retrieves a puzzle's permissions object from the permissions file.
+  Returns None for unlisted puzzles.
+  """
+  perms = get_current_permissions()
+  return perms["puzzles"].get(puzzle_id, None)
 
-def get_permisisons(user_id):
+def set_permissions(puzzle_id, perm_obj):
   """
-  Retrieves a user's permissions object from the permissions database. Returns
-  None for unlisted users.
+  Resets a puzzle's permissions entirely. Adds an entry to the
+  permissions file if necessary. THIS OPERATION MODIFIES THE PERMISSIONS
+  FILE, so be careful about not using it concurrently across multiple
+  contexts. Returns True on success and False on failure.
   """
-  conn = get_perm_db_connection()
-  cur = conn.execute(
-    "SELECT permissions FROM permissions WHERE username = ?;",
-    (user_id,)
-  )
-  results = list(cur.fetchall())
-  conn.close()
-  if len(results) <= 0:
-    return None
+  perms = get_current_permissions()
+  perms["puzzles"][puzzle_id] = perm_obj
+
+  return safely_overwrite_permissions_file(perms)
+
+
+def deny_permission(puzzle_id, user_id):
+  """
+  Adds an exception that prevents the given user from viewing the given
+  puzzle. Returns True if it succeeds and False if it fails.
+  """
+  pzperms = get_permisisons(puzzle_id)
+  if pzperms == None:
+    # Create a new permissions object for this puzzle
+    perms["puzzles"][puzzle_id] = { "allow": False, "deny": [ user_id ] }
+  elif user_id in pzperms.get("deny", []):
+    return True # already denied; no action requried
   else:
-    try:
-      return json.loads(results[0][0])
-    except:
-      print(
-        "Warning: error fetching or parsing permissions for  user '{}'".format(
-          user_id
-        )
-      )
-      return None
+    # Add to deny list
+    pzperms["deny"].append(user_id)
 
-def set_permissions(user_id, perm_obj):
+  return set_permissions(puzzle_id, pzperms)
+
+def reinstate_permission(puzzle_id, user_id):
   """
-  Resets a user's permissions entirely. Adds a non-admin entry to the
-  permissions database if necessary. Returns True if it succeeds or False if it
-  fails.
+  Removes the given user from the deny list for the given puzzle.
   """
+  pzperms = get_permisisons(puzzle_id)
+  if pzperms == None:
+    return True # no work to be done, as puzzle doesn't have a deny list yet
+
+  if user_id not in pzperms.get("deny", []):
+    return True # no work to be done, wasn't on the deny list
+  else:
+    pzperms["deny"].remove(user_id)
+
+  return set_permissions(puzzle_id, pzperms)
+
+
+def has_permission(puzzle_id, user_id):
+  """
+  Retrieves a puzzle's permissions object from the permissions file, and
+  checks whether the user has permission to view it. Returns True or
+  False. Admin accounts have permission to view all puzzles, and if the
+  user_id is None, it always returns False.
+  """
+  if is_admin(user_id):
+    return True
+  elif user_id == None:
+    return False
+
+  # Actually check puzzle permissions:
+  pzperms = get_permisisons(puzzle_id)
+  allow = pzperms.get("allow", False)
+  if allow == True:
+    deny = pzperms.get("deny", [])
+    return user_id not in deny
+  elif isinstance(allow, list):
+    return user_id in allow
+  else: # allow is false or some other unexpected value
+    return False
+
+def set_admin(user_id, admin=True):
+  """
+  Promotes the user to an admin, if admin is True, or demotes them to a
+  normal user, if admin is False. Returns True if it succeeds and False
+  if it fails.
+  """
+  perms = get_current_permissions()
+  if admin:
+    if user_id not in perms["admins"]:
+      perms["admins"].append(user_id)
+      return safely_overwrite_permissions_file(perms)
+    # otherwise don't need to do anything, user is already an admin
+  else:
+    if user_id in perms["admins"]:
+      perms["admins"].remove(user_id)
+      return safely_overwrite_permissions_file(perms)
+    # otherwise don't need to do anything, user is already NOT an admin
+
+def get_current_permissions():
+  """
+  Re-reads the permissions file to get up-to-date info on who is allowed
+  to view which puzzles. Returns default permissions if trouble is
+  encountered loading the permissions file.
+  """
+  pf = app.config.get("PERMISSIONS_FILE", "permissions.json")
+  try:
+    with open(pf, 'r') as fin:
+      perms = json.load(fin)
+    if "admins" not in perms:
+      perms["admins"] = []
+    if "puzzles" not in perms:
+      perms["puzzles"] = {}
+    return perms
+  except Exception as e:
+    if sys.version_info >= (3, 5):
+      tbe = traceback.TracebackException.from_exception(e)
+      print("Error loading permissions:\n" + '\n'.join(tbe.format()))
+    else:
+      print("Error loading permissions:")
+      traceback.print_exception(type(e), e, sys.last_traceback)
+    return DEFAULT_PERMISSIONS
+
+def safely_overwrite_permissions_file(perms):
+  """
+  Attempts to overwrite the permissions file with new permissions, but
+  respects a simple file-based lock. Not really much safer than a raw
+  write (there's still a potential race condition, for example), but
+  offers at least a modicum of corruption protection.
+  """
+  pf = app.config.get("PERMISSIONS_FILE", "permissions.json")
+  lf = pf + ".lock"
+  if os.path.exists(lf):
+    return False
+
+  # Convert JSON -> string first to save time we need the lock for
+  perms_string = json.dumps(perms, indent=2, separators=(',', ': '))
 
   try:
-    perm_string = json.dumps(perm_obj)
+    with open(lf, 'w') as fout:
+      fout.write("")
   except:
-    print(
-      (
-        "Warning: user permissions not set for '{}' because object couldn't "
-      + "be serialized:\n{}"
-      ).format(user_id, repr(perm_obj))
-    )
+    print("Failed to create lock file '{}'.".format(lf))
     return False
 
-  conn = get_perm_db_connection()
-  cur = conn.execute(
-    "SELECT * FROM permissions WHERE username = ?;",
-    (user_id,)
-  )
-  if len(list(cur.fetchall())) > 0: # user already exists
-    conn.execute(
-      "UPDATE permissions SET permissions = ? WHERE username = ?;",
-      (perm_string, user_id,)
-    )
-    conn.commit()
-  else: # new user
-    conn.execute(
-      "INSERT INTO permissions VALUES (?, ?, ?);",
-      (user_id, 'False', perm_string)
-    )
-    conn.commit()
-  conn.close()
-  return True
-
-def grant_permission(user_id, action, item):
-  """
-  Adds permission for the given user to take the given action. Returns True if
-  it succeeds and False if it fails.
-  """
-  perms = get_permisisons(user_id)
-  if perms == None:
-    print(
-      (
-        "Warning: permission {}:{} not added for '{}' because of trouble "
-      + "retrieving permissions for that user."
-      ).format(action, item, user_id)
-    )
-    return False
-
-  if action not in perms:
-    perms[action] = { item: True }
-  else:
-    perms[action][item] = True
-
-  # Update permissions object in the database
-  return set_permissions(user_id, perms)
-
-def revoke_permission(user_id, action, item):
-  """
-  Revokes permission for the given user to take the given action.
-  """
-  perms = get_permisisons(user_id)
-  if perms == None:
-    print(
-      (
-        "Warning: permission {}:{} not revoked for '{}' because of trouble "
-      + "retrieving permissions for that user."
-      ).format(action, item, user_id)
-    )
-    return False
-
-  if action not in perms:
-    return # already didn't have it
-  else:
-    del perms[action][item]
-
-  # Update permissions object in the database
-  return set_permissions(user_id, perms)
-
-
-def has_permission(user_id, action, item):
-  """
-  Retrieves a user's permissions object from the permissions database, and
-  checks whether the user has permission to perform the given action on the
-  given item. Returns True or False. Admin accounts have permission to do
-  everything, and if the user_id is None, it always returns False.
-  """
-  if user_id != None:
-    conn = get_perm_db_connection()
-    cur = conn.execute(
-      "SELECT permissions, is_admin FROM permissions WHERE username = ?;",
-      (user_id,)
-    )
-    results = list(cur.fetchall())
-    conn.close()
-    if len(results) >= 1:
-      is_admin = results[0][1] == "True"
-      try:
-        perm_obj = json.loads(results[0][0])
-      except:
-        print(
-          "Warning: error parsing permissions for user '{}'".format(user_id)
-        )
-        perm_obj = None
-  
-      if (
-        is_admin # can do anything
-     or (perm_obj and action in perm_obj and perm_obj[action].get(item, False))
-      ):
-        return True
-  
-  # If we fall out, check global permissions
-  conn = get_perm_db_connection()
-  cur = conn.execute(
-    'SELECT permissions FROM permissions WHERE username = "__all__";'
-  )
-  results = list(cur.fetchall())
-  conn.close()
-  if len(results) > 0:
+  # here we "have" the lock (but of course there's a race condition above)
+  try:
+    with open(pf, 'w') as fout:
+      fout.write(perms_string)
+  except:
+    print("Failed to write to permissions file '{}'.".format(pf))
     try:
-      perm_obj = json.loads(results[0][0])
+      os.remove(lf)
     except:
-      print(
-        "Warning: error parsing permissions for '__all__':\n{}".format(
-          results[0][0]
-        )
-      )
-      perm_obj = None
-
-    return (
-      perm_obj != None
-  and action in perm_obj
-  and perm_obj[action].get(item, False)
-    )
-
-  return False
-
-def set_admin(user_id, admin='True'):
-  """
-  Sets the 'admin' property of the given user, by default making them an admin.
-  Any value besides 'True' will be treated as a regular user. Prints a warning
-  message and returns False if the given user doesn't already exist.
-  """
-  conn = get_perm_db_connection()
-  # Check if the user already exists: 
-  cur = conn.execute(
-    "SELECT * FROM permissions WHERE username = ?;",
-    (user_id,)
-  )
-  if len(list(cur.fetchall())) == 0: # user doesn't exists
-    print(
-      "Attempt to set admin status of non-existent user '{}'.".format(user_id)
-    )
-    conn.close()
+      print("Failed to clean up lock file '{}'.".format(lf))
     return False
-  else:
-    conn.execute(
-      "UPDATE permissions SET is_admin = ? WHERE username = ?;",
-      (admin, user_id)
-    )
-    conn.commit()
 
-  conn.close()
+  try:
+    os.remove(lf)
+  except:
+    print("Failed to clean up lock file '{}'.".format(lf))
+    return False
+
+  # We succeeded!
   return True
 
-def add_user(user_id, is_admin='False'):
-  """
-  Adds the given user to the permissions database. Set is_admin to the string
-  'True' to make the user an administrator. Prints a warning and returns False
-  if the user already exists.
-  """
-  conn = get_perm_db_connection()
-  # Check if the user already exists: 
-  cur = conn.execute(
-    "SELECT * FROM permissions WHERE username = ?;",
-    (user_id,)
-  )
-  results = list(cur.fetchall())
-  conn.close()
-  if len(results) > 0: # user already exists
-    print(
-      "Attempt to create user '{}' who already exists.".format(user_id)
-    )
-    return False
-  else:
-    if not set_permissions(user_id, {}): # creates user automatically
-      return False
-    if is_admin != 'False': # that's the default in set_permissions
-      if not set_admin(user_id, is_admin):
-        return False
-
-  return True
-
-def get_sol_db_connection():
-  """
-  Gets a connection to the solutions database.
-  """
-  conn = sqlite3.connect(SOL_DATABASE)
-  conn.row_factory = sqlite3.Row # return results as Row objects
-  return conn
-
-def get_perm_db_connection():
-  """
-  Gets a connection to the permissions database.
-  """
-  conn = sqlite3.connect(PERM_DATABASE)
-  conn.row_factory = sqlite3.Row # return results as Row objects
-  return conn
 
 #--------------#
 # Startup Code #
 #--------------#
 
 if __name__ == "__main__":
-  # Set up databases if they doesn't already exist:
-  if not os.path.exists(SOL_DATABASE):
-    conn = get_sol_db_connection()
-    conn.execute(SOL_SCHEMA)
-    conn.commit()
-    conn.close()
-  if not os.path.exists(PERM_DATABASE):
-    conn = get_perm_db_connection()
-    conn.execute(PERM_SCHEMA)
-    conn.commit()
-    conn.close()
   #app.run('localhost', 1947, ssl_context=('cert.pem', 'key.pem'))
   #app.run('0.0.0.0', 1947, ssl_context=('cert.pem', 'key.pem'))
   app.run('localhost', 1947)
