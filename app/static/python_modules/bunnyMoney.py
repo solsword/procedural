536b57962d24f78109eefececc7059dfa367f8da
CS111 <sbuck@wellesley.edu>
2019-09-26 16:43:15 -0400

    Upgraded list.py to use the new non-database functions, and to include a missing mode to see whch students *haven't* submitted a particular puzzle (or set of puzzles).
    -Peter Mawhorter

---
+++ app/static/python_modules/bunnyMoney.py
@@ -0,0 +1,11 @@
+from cs1graphics import *
+
+# This is a stub file to speed up testing since graphics are not actually
+# displayed.
+
+def makeBunnyMoney():
+    result = Layer()
+    thing = Rectangle(600, 300, Point(300, 150))
+    result.add(thing)
+
+    return result
